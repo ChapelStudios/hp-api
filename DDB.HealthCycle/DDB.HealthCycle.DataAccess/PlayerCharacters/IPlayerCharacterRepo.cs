4423adcd1103d56c7435a758529fda7ea7b8eddb
Jake Blackwell <jblackwell@qw-corp.com>
2024-07-26 18:11:19 -0500

    added repo for saving and upserting characters, needs soft-delete;

---
+++ DDB.HealthCycle/DDB.HealthCycle.DataAccess/PlayerCharacters/IPlayerCharacterRepo.cs
@@ -0,0 +1,30 @@
+﻿using DDB.HealthCycle.Models.DataModels;
+using DDB.HealthCycle.Models.DTO;
+
+namespace DDB.HealthCycle.DataAccess.PlayerCharacters;
+public interface IPlayerCharacterRepo
+{
+    /// <summary>
+    /// Gather a Player Character's record by their ID
+    /// </summary>
+    /// <param name="id">Id of the PlayerCharacter</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if the ID doesn't return any results.</exception>
+    PlayerCharacterRecord GetCharacterRecordById(string id);
+
+    /// <summary>
+    /// Gather a Player Character's data by their ID
+    /// </summary>
+    /// <param name="id">Id of the PlayerCharacter</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if the ID doesn't return any results.</exception>
+    /// <exception cref="FormatException">Thrown if the JSON data cannot be deserialized.</exception>
+    PlayerCharacter GetCharacterById(string id);
+
+    /// <summary>
+    /// Either adds or updates a PlayerCharacter as appropriate.
+    /// </summary>
+    /// <param name="playerCharacter">Character data to update.</param>
+    /// <returns>A bool letting you know if the data was saved correctly.</returns>
+    bool UpsertPlayerCharacter(PlayerCharacter playerCharacter);
+}
---
+++ DDB.HealthCycle/DDB.HealthCycle.DataAccess/PlayerCharacters/PlayerCharacterRepo.cs
@@ -0,0 +1,76 @@
+﻿using DDB.HealthCycle.Data;
+using DDB.HealthCycle.DataAccess.DateTimeProvider;
+using DDB.HealthCycle.Models.DataModels;
+using DDB.HealthCycle.Models.DTO;
+using Microsoft.Extensions.Logging;
+
+namespace DDB.HealthCycle.DataAccess.PlayerCharacters;
+
+public class PlayerCharacterRepo(
+    PlayerCharacterContext _pcContext,
+    IDateTimeProvider _dateTimeProvider,
+    ILogger<PlayerCharacterRepo> _logger
+) : IPlayerCharacterRepo
+{
+    /// <summary>
+    /// Gather a Player Character's record by their ID
+    /// </summary>
+    /// <param name="id">Id of the PlayerCharacter</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if the ID doesn't return any results.</exception>
+    public PlayerCharacterRecord GetCharacterRecordById(string id)
+    {
+        return _pcContext.PlayerCharacterRecords
+            .First(c => c.Id == id) ?? throw new ArgumentOutOfRangeException(nameof(id), "Character not found");
+    }
+
+    /// <summary>
+    /// Gather a Player Character's data by their ID
+    /// </summary>
+    /// <param name="id">Id of the PlayerCharacter</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if the ID doesn't return any results.</exception>
+    /// <exception cref="FormatException">Thrown if the JSON data cannot be deserialized.</exception>
+    public PlayerCharacter GetCharacterById(string id)
+    {
+        var record = GetCharacterRecordById(id);
+        return record.Unpack() ?? throw new FormatException("Character json corrupted");
+    }
+
+    /// <summary>
+    /// Either adds or updates a PlayerCharacter as appropriate.
+    /// </summary>
+    /// <param name="playerCharacter">Character data to update.</param>
+    /// <returns>A bool letting you know if the data was saved correctly.</returns>
+    public bool UpsertPlayerCharacter(PlayerCharacter playerCharacter)
+    {
+        try
+        {
+            bool isSuccess = false;
+            try
+            {
+                var existing = GetCharacterRecordById(playerCharacter.Id);
+                existing.Pack(playerCharacter);
+                existing.Updated = _dateTimeProvider.UtcNow;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                var newPC = new PlayerCharacterRecord(playerCharacter)
+                {
+                    Updated = _dateTimeProvider.UtcNow
+                };
+                _pcContext.Add(newPC);
+            }
+            finally
+            {
+                isSuccess = _pcContext.SaveChanges() > 0;
+            }
+            return isSuccess;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            throw;
+        }
+    }
+}
