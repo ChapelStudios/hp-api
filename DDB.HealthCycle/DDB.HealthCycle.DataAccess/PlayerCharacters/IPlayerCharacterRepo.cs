28805bf0f14454ede5eee89114d5ad76eee447e4
Jake Blackwell <jblackwell@qw-corp.com>
2024-07-27 00:50:07 -0500

    updated repo to be async; updated logging msg;

--- DDB.HealthCycle/DDB.HealthCycle.DataAccess/PlayerCharacters/IPlayerCharacterRepo.cs
+++ DDB.HealthCycle/DDB.HealthCycle.DataAccess/PlayerCharacters/IPlayerCharacterRepo.cs
@@ -10,7 +10,7 @@ public interface IPlayerCharacterRepo
     /// <param name="id">Id of the PlayerCharacter</param>
     /// <returns></returns>
     /// <exception cref="ArgumentOutOfRangeException">Thrown if the ID doesn't return any results.</exception>
-    PlayerCharacterRecord GetCharacterRecordById(string id);
+    Task<PlayerCharacterRecord> GetCharacterRecordById(string id);
 
     /// <summary>
     /// Gather a Player Character's data by their ID
@@ -19,12 +19,12 @@ public interface IPlayerCharacterRepo
     /// <returns></returns>
     /// <exception cref="ArgumentOutOfRangeException">Thrown if the ID doesn't return any results.</exception>
     /// <exception cref="FormatException">Thrown if the JSON data cannot be deserialized.</exception>
-    PlayerCharacter GetCharacterById(string id);
+    Task<PlayerCharacter> GetCharacterById(string id);
 
     /// <summary>
     /// Either adds or updates a PlayerCharacter as appropriate.
     /// </summary>
     /// <param name="playerCharacter">Character data to update.</param>
     /// <returns>A bool letting you know if the data was saved correctly.</returns>
-    bool UpsertPlayerCharacter(PlayerCharacter playerCharacter);
+    Task<bool> UpsertPlayerCharacter(PlayerCharacter playerCharacter);
 }
--- DDB.HealthCycle/DDB.HealthCycle.DataAccess/PlayerCharacters/PlayerCharacterRepo.cs
+++ DDB.HealthCycle/DDB.HealthCycle.DataAccess/PlayerCharacters/PlayerCharacterRepo.cs
@@ -2,6 +2,7 @@
 using DDB.HealthCycle.DataAccess.DateTimeProvider;
 using DDB.HealthCycle.Models.DataModels;
 using DDB.HealthCycle.Models.DTO;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DDB.HealthCycle.DataAccess.PlayerCharacters;
@@ -18,10 +19,10 @@ public class PlayerCharacterRepo(
     /// <param name="id">Id of the PlayerCharacter</param>
     /// <returns></returns>
     /// <exception cref="ArgumentOutOfRangeException">Thrown if the ID doesn't return any results.</exception>
-    public PlayerCharacterRecord GetCharacterRecordById(string id)
+    public async Task<PlayerCharacterRecord> GetCharacterRecordById(string id)
     {
-        return _pcContext.PlayerCharacterRecords
-            .First(c => c.Id == id) ?? throw new ArgumentOutOfRangeException(nameof(id), "Character not found");
+        return await _pcContext.PlayerCharacterRecords
+            .FirstAsync(c => c.Id == id) ?? throw new ArgumentOutOfRangeException(nameof(id), "Character not found");
     }
 
     /// <summary>
@@ -31,9 +32,9 @@ public class PlayerCharacterRepo(
     /// <returns></returns>
     /// <exception cref="ArgumentOutOfRangeException">Thrown if the ID doesn't return any results.</exception>
     /// <exception cref="FormatException">Thrown if the JSON data cannot be deserialized.</exception>
-    public PlayerCharacter GetCharacterById(string id)
+    public async Task<PlayerCharacter> GetCharacterById(string id)
     {
-        var record = GetCharacterRecordById(id);
+        var record = await GetCharacterRecordById(id);
         return record.Unpack() ?? throw new FormatException("Character json corrupted");
     }
 
@@ -42,14 +43,14 @@ public class PlayerCharacterRepo(
     /// </summary>
     /// <param name="playerCharacter">Character data to update.</param>
     /// <returns>A bool letting you know if the data was saved correctly.</returns>
-    public bool UpsertPlayerCharacter(PlayerCharacter playerCharacter)
+    public async Task<bool> UpsertPlayerCharacter(PlayerCharacter playerCharacter)
     {
         try
         {
             bool isSuccess = false;
             try
             {
-                var existing = GetCharacterRecordById(playerCharacter.Id);
+                var existing = await GetCharacterRecordById(playerCharacter.Id);
                 existing.Pack(playerCharacter);
                 existing.Updated = _dateTimeProvider.UtcNow;
             }
@@ -63,13 +64,13 @@ public class PlayerCharacterRepo(
             }
             finally
             {
-                isSuccess = _pcContext.SaveChanges() > 0;
+                isSuccess = (await _pcContext.SaveChangesAsync()) > 0;
             }
             return isSuccess;
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, ex.Message);
+            _logger.LogError(ex, "Unknown exception while upserting Character Id: {playerCharacterId}", playerCharacter.Id);
             throw;
         }
     }
